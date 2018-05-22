969c6acffe3f67ba98ce407a7b9eb45f2bb1fb05
ReCoal <recoalcommunity@gmail.com>
2018-05-22 18:53:39 +0530

    Update version.cpp

--- xmrstak/version.cpp
+++ xmrstak/version.cpp
@@ -17,8 +17,8 @@
 #define BACKEND_TYPE unknown
 #endif
 
-#define XMR_STAK_NAME "xmr-stak"
-#define XMR_STAK_VERSION "2.4.3"
+#define XMR_STAK_NAME "recoal-miner"
+#define XMR_STAK_VERSION "0.2.0"
 
 #if defined(_WIN32)
 #define OS_TYPE "win"
